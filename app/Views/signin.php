017a5bf6abf233b24b7f1b1442b7f368511e8a94
reynave <felix.renaldi@gmail.com>
2023-09-20 18:47:00 +0700

    update add md5

--- app/Views/signin.php
+++ app/Views/signin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +8,13 @@
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Poppins:wght@400;500;700;900&display=swap"
+    <link  href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Poppins:wght@400;500;700;900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link href="<?= base_url()?>/assets/signin/style.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/signin/style.css" rel="stylesheet">
     <script src="https://accounts.google.com/gsi/client" async></script>
 
 </head>
@@ -29,7 +27,7 @@
                 <div class="row">
                     <div class="col-12 text-center">
 
-                        <img src="<?= base_url()?>/assets/img/mirrel.png" class="w-75">
+                        <img src="<?= base_url() ?>/assets/img/mirrel.png" class="w-75">
                         <div class="mb-3">
                             by <b>Trader</b> for trader
                         </div>
@@ -41,13 +39,7 @@
                     </div>
 
                     <div class="col-12">
-                        <div class="signin-content text-center  mt-4">
-
-                            <div class="center-line my-4">
-                                <div class="line"></div>
-                                <div class="mx-3"><small>Sign In With</small></div>
-                                <div class="line"></div>
-                            </div>
+                        <div class="signin-content text-center  mt-1">
                             <div class="mb-3 text-center">
                                 <div id="g_id_onload"
                                     data-client_id="480785640282-pri4rt3btbv7gtv0f68r0aoj96v44559.apps.googleusercontent.com"
@@ -61,30 +53,58 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="mb-3 d-grid">
-                            <button type="button" class="btn btn-light border" disabled><i class="bi bi-facebook"></i>
-                                <small>Sign in with Facebook</small></button>
-                        </div>
+                    </div>
 
-                        <div class="mb-3 d-grid">
-                            <button type="button" class="btn btn-light border" disabled><i class="bi bi-twitter"></i>
-                                <small>Sign in with Twitter</small></button>
+                    <div class="col-12">
+                        <div class="center-line my-3">
+                            <div class="line"></div>
+                            <div class="mx-3"><small>Or Manual Sign In With</small></div>
+                            <div class="line"></div>
                         </div>
                     </div>
+
+                    <div class="col-12   ">
+                        <div class="form-signin text-center">
+                            <div class="form-floating">
+                                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                                <label for="email">Email address</label>
+                            </div>
+
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="password" placeholder="Password">
+                                <label for="password">Password</label>
+                            </div>
+
+                            <div class="d-grid">
+                                <button type="button" id="submit-btn" class="btn btn-lg btn-dark rounded-0">Continue</a>
+                            </div>
+                            <div class="py-2">
+                                <div class="warning" id="email-error"></div>
+                                <div class="warning" id="password-error"></div>
+                                <div class="warning" id="warning-message"></div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
         </div>
 
     </div>
-    </div>
-
+   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
-    <script src="<?= base_url()?>signin/config.js"></script>
-    <script src="<?= base_url()?>assets/signin/app.js"></script>
+
+    <script src="<?= base_url() ?>assets/plugin/md5.min.js"></script>
+    <script src="<?= base_url() ?>signin/config.js"></script>
+    <script src="<?= base_url() ?>assets/signin/app.js"></script>
+    <script>
+        document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+            ':35729/livereload.js?snipver=1"></' + 'script>')
+    </script>
 </body>
 
 </html>
--- public/assets/signin/app.js
+++ public/assets/signin/app.js
@@ -6,30 +6,93 @@ function handleCredentialResponse(res) {
         $.ajax({
             type: "POST",
             url: api + "login/auth0",
-            data: { 
-                data: res['credential'] 
+            data: {
+                data: res['credential']
             },
             dataType: 'json',
             success: function (data, textStatus, jqXHR) {
                 console.log(data);
-                localStorage.setItem("app.mirrel.com",data['token']);
-                setTimeout(()=>{
-                    location.href =  home+"/login?id="+data['accountTokenId'];
-                },500)
+                localStorage.setItem("app.mirrel.com", data['token']);
+                setTimeout(() => {
+                    location.href = home + "/login?id=" + data['accountTokenId'];
+                }, 500)
             },
-            error: function(e){
+            error: function (e) {
                 console.log(e.responseJSON.message);
                 console.log(e);
             }
 
         });
 
-      
+
 
     } else {
         console.log("clientId not valid");
     }
 }
+function encryptToMD5(input) {
+    // Import library MD5.js 
+    // Enkripsi input menjadi MD5
+    var encrypted = MD5(input);
 
- 
+    return encrypted;
+}
+$(document).ready(function () {
+    $("#submit-btn").click(function () {
+        // Mengambil nilai inputan email dan password
+        var email = $("#email").val();
+        var password = $("#password").val();
 
+        // Reset pesan error dan peringatan
+        $("#email-error").text("");
+        $("#password-error").text("");
+        $("#warning-message").text("");
+
+        // Validasi email tidak boleh kosong dan harus sesuai format email
+        if (email === "") {
+            $("#email-error").text("Email wajib diisi.");
+        } else if (!isValidEmail(email)) {
+            $("#email-error").text("Format email tidak valid.");
+        }
+
+        // Validasi password tidak boleh kosong
+        if (password === "") {
+            $("#password-error").text("Password wajib diisi.");
+        }
+
+        // Tampilkan pesan peringatan jika tidak valid
+        if ((email === "" || !isValidEmail(email)) || password === "") {
+            $("#warning-message").text("Harap isi semua kolom dengan benar.");
+        } else {
+            var encryptedPassword = encryptToMD5(password);
+            const body = { email: email, password: encryptedPassword };
+            console.log(body);
+
+            $.ajax({
+                type: "POST",
+                url: api + "login/signIn",
+                data: body,
+                success: function (data) {
+                    // Handle respons dari server di sini
+                    console.log(data);
+                    if (data['error'] == false) {
+                        localStorage.setItem("app.mirrel.com", data['token']);
+
+                        setTimeout(() => {
+                            location.href = home + "login?id=" + data['accountTokenId'];
+                        }, 500)
+                    }
+                    $("#warning-message").text(data['post']);
+
+                },
+                dataType: 'json'
+            });
+        }
+    });
+
+    // Fungsi untuk validasi format email
+    function isValidEmail(email) {
+        var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailRegex.test(email);
+    }
+});
